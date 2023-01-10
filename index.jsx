609f5e4d8df0da2c8b4ae2d3343aede6519bb9cd
CetO-hub <skynet@SkyNet.localdomain>
2023-01-10 14:33:41 +0100

    feat: Add backbone structure

---
+++ index.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.scss";
+
+const MyFlixApplication = () => {
+  return (
+    <div className="my-flix">
+      <div>Good morning</div>
+    </div>
+  );
+};
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+root.render(MyFlixApplication);
